fa7707da18907897b435e1ab0b4f92fb2fb06963
wu-tang <ok.names.ugaman@gmail.com>
2017-08-26 11:26:18 +0900

    8章を何も学ぶことなくただコピペして終わらせた

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,9 +5,10 @@ require "minitest/reporters"
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests
-  # in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  # テストユーザーがログイン中の場合にtrueを返す
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end

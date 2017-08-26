a88ac1b6156a49a10672724beef1a640b9b0fc73
wu-tang <ok.names.ugaman@gmail.com>
2017-08-26 11:01:09 +0900

    Use SSL and the Puma webserver in production

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,14 +1,9 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get users_index_url
+
+  test "should get new" do
+    get signup_path
     assert_response :success
   end
-
-  test "should get show" do
-    get users_show_url
-    assert_response :success
-  end
-
 end

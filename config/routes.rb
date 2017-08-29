537a3f5db564f3e9b7671a68272fa4b80185217e
Kentaro Kuribayashi <kentarok@gmail.com>
2017-08-29 16:29:06 +0900

    add nenpyo page

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,11 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  resources :users
+
+  resources :users do
+    ### XXX
+    member do
+      get :nenpyo
+    end
+  end
 end
